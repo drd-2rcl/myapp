dc4f40401e48bff5375e5e669d77e227d56e03fc
Diogo <drd.rclrd@gmail.com>
2019-09-19 17:41:51 -0300

    added limit for title

--- db/migrate/20190919203944_create_ads.rb
+++ db/migrate/20190919203944_create_ads.rb
@@ -1,7 +1,7 @@
 class CreateAds < ActiveRecord::Migration[6.0]
   def change
     create_table :ads do |t|
-      t.string :title
+      t.string :title, limit: 255
       t.text :description
       t.references :member, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
