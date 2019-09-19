4fee09b8e89fd0a3c29f26b5603aa464e76563e6
Diogo <drd.rclrd@gmail.com>
2019-09-19 17:06:39 -0300

    added values for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando as CATEGORIAS..."
+categories = [
+  "Animais e acessórios",
+  "Esportes",
+  "Para a sua casa",
+  "Eletrônicos e hobbies",
+  "Músicas e hobbies",
+  "bebês e crianças",
+  "Moda e beleza",
+  "Veículos e barcos",
+  "Imóveis",
+  "Empregos e negócios"
+]
+
+categories.each do |category|
+  Category.find_or_create_by(description: category)
+end
+
+puts "CATEGORIAS cadastradas com sucesso!"
