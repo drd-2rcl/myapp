716c8be5d3f5e0aa9fc2ef95f50f6d7744dcf500
Diogo <drd.rclrd@gmail.com>
2019-09-19 17:38:56 -0300

    added config for view log in console after up server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Foreman fix
+  $stdout.sync = true
+
   # Devise Config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
